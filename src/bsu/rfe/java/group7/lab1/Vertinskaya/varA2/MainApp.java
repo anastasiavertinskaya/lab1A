55b0d0db6f141a3f72b007a0e25d92cb76d20b9c
anastasiavertinskaya <anvertinskaya049@gmail.com>
2020-11-12 21:50:52 +0300

    Добавлен подсчет съеденных продуктов

--- src/bsu/rfe/java/group7/lab1/Vertinskaya/varA2/MainApp.java
+++ src/bsu/rfe/java/group7/lab1/Vertinskaya/varA2/MainApp.java
@@ -3,26 +3,33 @@ package bsu.rfe.java.group7.lab1.Vertinskaya.varA2;
 public class MainApp {
     public static void main(String[] args) {
         Food[] breakfast = new Food[20];
+        int sumApple = 0;
+        int sumPie = 0;
+        int sumCheese = 0;
         int i = 0;
         for (String arg : args) {
             String[] parts = arg.split(("/"));
             if (parts[0].equals("Cheese")) {
-                breakfast[i] = new Cheese();
+                Cheese cheese = new Cheese();
+                cheese.consume();
+                sumCheese++;
+
             } else if (parts[0].equals("Apple")) {
-                breakfast[i] = new Apple(parts[1]);
+                Apple apple = new Apple(parts[1]);
+                apple.consume();
+                sumApple++;
             } else if (parts[0].equals("Pie")) {
-                breakfast[i] = new Pie(parts[1]);
+                Pie pie = new Pie(parts[1]);
+                pie.consume();
+                sumPie++;
             }
             i++;
         }
         System.out.println("Завтрак: ");
-        for (Food item : breakfast) {
-            if (item != null) {
-                item.consume();
-            } else {
-                break;
-            }
-        }
+        System.out.println("Количество съеденных продуктов:  " + (sumApple+sumPie+sumCheese));
+        System.out.println("Яблок съедено: " + sumApple);
+        System.out.println("Пирогов съедено: " + sumPie);
+        System.out.println("Сыра съедено: " + sumCheese);
         System.out.println("Всего хорошего!");
     }
 }
